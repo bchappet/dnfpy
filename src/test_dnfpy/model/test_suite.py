52b52bc598040126c75033dc5b4ff9bf75eb03f6
Benoit <bdevangel@gmail.com>
2014-11-25 21:55:04 +0100

    In fact we don't have to add the tests to the test suite to execute them

--- src/test_dnfpy/model/test_suite.py
+++ src/test_dnfpy/model/test_suite.py
@@ -1,14 +1,6 @@
 import unittest
 from test_dnfpy.model import *
-def suite():
-        suite = unittest.TestSuite()
-        suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestInputMap))
-        suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestFieldMap))
-        suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestLateralWeightsMap))
-
-        return suite
 
 if __name__ == '__main__':
-        test_suite = suite()
         unittest.main()
 
