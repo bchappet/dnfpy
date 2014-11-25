c7eefc76f56d3b572479dd5ef5a781426c341d70
Benoit <bdevangel@gmail.com>
2014-11-25 21:58:18 +0100

    Added testActivationMap and corrected errors in activationMap

--- src/dnfpy/model/activationMap.py
+++ src/dnfpy/model/activationMap.py
@@ -4,8 +4,8 @@ from dnfpy.core.map2D import Map2D
 class ActivationMap(Map2D):
     def _compute(self,model,field,th):
         if model == 'cnft':
-            self.data = np.maximum(field,0)
+            self._data = np.maximum(field,0)
         elif model == 'spike':
-            self.data = np.where(field > th,1.,0.)
+            self._data = np.where(field > th,1.,0.)
         else:
             print " Invalid model option : %s" % model
