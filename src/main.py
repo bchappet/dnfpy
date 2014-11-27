403325d53bbedaed6e304fcb084a9886758e882b
Benoit <bdevangel@gmail.com>
2014-11-27 11:50:15 +0100

    main.py no takes the name of the model class as first arg to generate the model

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 import sys
 from modelDNF import ModelDNF
+from modelDNFCam import ModelDNFCam
 from dnfpy.view.dynamicViewQt import DisplayMapsQt
 from dnfpy.controller.runner import Runner
 from PyQt4 import QtGui
+import parser
 
-context = sys.argv[1] #arguments of the model
-timeEnd = sys.argv[2] # simulation end
+modelName = sys.argv[1]
+moduleName = modelName[0].lower() + modelName[1:]
+baz = sys.modules[moduleName]
+clazz = getattr(baz,modelName)
+
+
+
+context = sys.argv[2] #arguments of the model
+timeEnd = sys.argv[3] # simulation end
 
 params = eval(open(context,'r').read()) 
 
+
 app = QtGui.QApplication(sys.argv)
-model = ModelDNF(params)
+model = clazz(params)
 view = DisplayMapsQt(model)
 runner = Runner(model,view,timeEnd)
 
