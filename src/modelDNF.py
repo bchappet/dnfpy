5ab6c242bd949c05de49933c73609ddcee7d5b94
Benoit <bdevangel@gmail.com>
2014-11-27 02:48:35 +0100

    Removed useless code

--- src/modelDNF.py
+++ src/modelDNF.py
@@ -43,29 +43,4 @@ class ModelDNF(Model,Renderable):
     def getArraysDict(self):
         return dict(aff=self.aff.getData(),field=self.field.getData(),lat=self.lat.getData(),act=self.activation.getData())
 
-    
-def run(model,timeEnd):
-    """Will be the controller"""
-    simuTime = 0
-    while simuTime < timeEnd:
-        nextTime = model.getSmallestNextUpdateTime()
-        simuTime = nextTime
-        model.update(simuTime)
-def plot(model):
-    """Will be the view"""
-    graphix.plotMaps(model.getMapsToDisplay())
-    plt.show()
-    
-
-if __name__ == "__main__":
-    args = sys.argv[1] #name of the file containing the global argument dictionary
-    params = eval(open(sys.argv[1],'r').read()) #should separate args commandline, arg controler and arg view and arg model
-    model = Model(params)
-    run(model,params['time_end'])
-    plot(model)
-
-
-
-
-        
 
