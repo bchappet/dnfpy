b2347f364a46ec0dbff48799a7dfdc68e590a5cb
Benoit <bdevangel@gmail.com>
2015-01-08 22:37:31 +0100

    activation map can now have an user chosen type (dtype)

--- src/dnfpy/model/activationMap.py
+++ src/dnfpy/model/activationMap.py
@@ -2,13 +2,13 @@ import numpy as np
 from dnfpy.core.map2D import Map2D
 
 class ActivationMap(Map2D):
-    def __init__(self,name,size,dt=0.1,model='cnft',th=0.75,**kwargs):
+    def __init__(self,name,size,dt=0.1,model='cnft',th=0.75,dtype=np.float32,**kwargs):
         super(ActivationMap,self).__init__(name,
-            size=size,dt=dt,model=model,th=th,**kwargs)
-    def _compute(self,model,field,th):
+            size=size,dt=dt,model=model,th=th,dtype=dtype,**kwargs)
+    def _compute(self,model,field,th,dtype):
         if model == 'cnft':
-            self._data = np.maximum(field,0)
+            self._data = np.maximum(field,dtype(0))
         elif model == 'spike':
-            self._data = np.where(field > th,1.,0.)
+            self._data = np.where(field > th,dtype(1),dtype(0))
         else:
             print " Invalid model option : %s" % model
