ee269070394069e4741012df50f71ba9aa2bc021
Benoit <bdevangel@gmail.com>
2015-01-08 22:36:55 +0100

    constant map hold an np.array, and has is value on update

---
+++ src/dnfpy/core/constantMap.py
@@ -0,0 +1,13 @@
+from dnfpy.core.map2D import Map2D
+
+class ConstantMap(Map2D):
+    def __init__(self,name,size,value,**kwargs):
+        super(ConstantMap,self).__init__(name,size,dt=1e10,value=value,**kwargs)
+
+    def _compute(self,value):
+        self._data = value
+
+
+
+    def reset(self):
+        self._data = self.getArg("value")
