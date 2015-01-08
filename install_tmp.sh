4f010ca76389317498405372687604a45ab3f5bd
Benoit <bdevangel@gmail.com>
2015-01-08 22:30:58 +0100

    added the hardlib to install

--- install_tmp.sh
+++ install_tmp.sh
@@ -2,4 +2,5 @@
 #will add dnfpy and test_dnfpy package to PYTHONPATH
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:.:~/Dropbox/THESE/workspace_python/SDNF/dnfpy/src/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Dropbox/THESE/workspace_python/SDNF/dnfpy/src/dnfpy/cellular/cpp_implementation/
 
