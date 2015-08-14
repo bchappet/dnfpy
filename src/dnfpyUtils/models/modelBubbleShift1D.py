8b1c4546c3d406ddb90e3de8306cce672a983cd1
bchappet <bdevangel@gmail.com>
2015-08-14 16:10:39 +0200

    Introduceing the bubbleshifting model (todo scenario)

--- src/dnfpyUtils/models/modelBubbleShift1D.py
+++ src/dnfpyUtils/models/modelBubbleShift1D.py
@@ -5,18 +5,27 @@ from dnfpy.model.model import Model
 from dnfpy.model.mapDNFND import MapDNFND
 from dnfpy.core.constantMapND import ConstantMapND
 import numpy as np
+import dnfpy.core.utilsND as utils
+from dnfpy.core.funcMapND import FuncMapND
 
-class ModelDNF1D(Model,Renderable):
+class ModelBubbleShift1D(Model,Renderable):
+    """
+    We are trying to show that it is possible to push a bubble with a string inhibbition
+    """
     def initMaps(self,size=49,model="cnft",activation="step",nbStep=0,
-                 iExc=1.25,iInh=0.7,wExc=0.1,wInh=10.,alpha=10.,th=0.75,h=0,
+                 iExc=3.5,iInh=0.7,wExc=0.1,wInh=1.,alpha=10.,th=0.75,h=0,
                  ):
         """We initiate the map and link them"""
        # print("iExc : %s, iInh: %s, wExc %s, wInh %s"%(iExc,iInh,wExc,wInh))
         #Create maps
-        self.aff = InputMap("Inputs",size,periodStim=20,iStim1 = 0 )
+        self.aff = InputMap("Inputs",size,periodStim=20000,iStim1 = 0 )
         #input = np.zeros((size))
         #input[size/2] = 1
         #self.aff = ConstantMapND("Inputs",size,value=input)
+
+        self.inh = FuncMapND(utils.gaussNd,"inh",size,dt=0.1,center=size/2,
+                              wrap=True,intensity=-1.,width=0.1*size)
+        self.aff.addChildren(self.inh)
                
         self.field = MapDNFND("Potential",size,model=model,activation=activation,nbStep=nbStep, \
                         iExc=iExc,iInh=iInh,wExc=wExc,wInh=wInh,th=th,h=h)
@@ -27,7 +36,7 @@ class ModelDNF1D(Model,Renderable):
 
     #override Renderable
     def getArrays(self):
-        ret =  [self.aff,self.field,self.field.act,self.field.lat,self.field.kernel,]
+        ret =  [self.aff,self.field,self.field.act,self.field.lat,self.field.kernel,self.inh]
         return ret
 
     def onClick(self,mapName,x,y):
