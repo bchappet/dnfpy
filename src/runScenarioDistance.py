878c7da9cee8ab6ff4139e70572e957d377e6be9
plfleury <fleury.paul@laposte.net>
2015-08-28 18:26:10 +0200

    Finish Rapport

--- src/runScenarioDistance.py
+++ src/runScenarioDistance.py
@@ -13,17 +13,24 @@ scenarioClass = getClassFromName("ScenarioEpuckDistance","scenarios")
 
 model = modelClass(**parameterDictModel)
 
-tab=np.array([])
-for i in range(6,12):
-    dist=float(i)/100
+tabDist=np.array([])
+tabDpdt=np.array([])
+for i in range(60,120,5):
+    dist=float(i)/1000
     
     parameterDictScenario = {'dist':dist}
     scenario = scenarioClass(**parameterDictScenario)
+    
+    for i in range(2):
+        res = runner.launch(model,scenario,20)
+        print("res",res)
+        tabDist=np.append(tabDist,res[0])
+        tabDpdt=np.append(tabDpdt,res[1])
+    
+    
 
-    res = runner.launch(model,scenario,20)
-    print("res",res)
-    tab=np.append(tab,res)
+print("tabDist",tabDist)
+print("tabDpdt",tabDpdt)
 
-print("tab",tab)
     
     
