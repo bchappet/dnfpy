1f6fad811bf13b2c71ed689a648cf2e5bd16a444
bchappet <bdevangel@gmail.com>
2016-04-02 12:44:35 +0200

    add xp in historique2

--- src/experiments/historique2.fish
+++ src/experiments/historique2.fish
@@ -27,6 +27,7 @@ python3 runExperiment2.py --models "['ModelNSpike']" --kwmodel "{'cell':'Rsdnf',
 #rsdnf2  
 python3 runExperiment2.py --models "['ModelNSpike']" --kwmodel "{'cell':'Rsdnf2','reproductible':False,'nspike':1,'model':'spike','clkRatio':80}" --prefix "NSpike_rsdnf2Scenario_1spike"  --nbThread 8  --scenarios "['ScenarioNoise','ScenarioDistracters','ScenarioTracking']"
 python3 runExperiment2.py --models "['ModelNSpike']" --kwmodel "{'cell':'Rsdnf2','reproductible':False,'nspike':5,'model':'spike','clkRatio':80}" --prefix "NSpike_rsdnf2Scenario_5spike"  --nbThread 8  --scenarios "['ScenarioNoise','ScenarioDistracters','ScenarioTracking']"
+python3 runExperiment2.py --models "['ModelNSpike']" --kwmodel "{'cell':'Rsdnf2','reproductible':False,'nspike':10,'model':'spike','clkRatio':378}" --prefix "NSpike_rsdnf2Scenario_10spike"  --nbThread 8  --scenarios "['ScenarioNoise','ScenarioDistracters','ScenarioTracking']"
 python3 runExperiment2.py --models "['ModelNSpike']" --kwmodel "{'cell':'Rsdnf2','reproductible':False,'nspike':20,'model':'spike','clkRatio':500}" --prefix "NSpike_rsdnf2Scenario_20spike"  --nbThread 8  --scenarios "['ScenarioNoise','ScenarioDistracters','ScenarioTracking']"
 
 #sequence mixte
